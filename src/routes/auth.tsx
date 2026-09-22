fceb859799b23322b4ec6d1f6716efabc8aed229
saqar2030 <saqarksa@hotmail.com>
2026-09-22 12:21:43 +0300

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -2,7 +2,8 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { toast } from "sonner";
 import { supabase } from "@/integrations/supabase/client";
-import { lovable } from "@/integrations/lovable/index";
+
+
 
 export const Route = createFileRoute("/auth")({
   head: () => ({
